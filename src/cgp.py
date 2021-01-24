6ad812f1f85791f9f58bcef78c4b9dfeedcef663
CSjianbel <jiankarlo99@gmail.com>
2021-01-24 21:45:34 +0800

    Command Line Args Parsing : Authentication and Repo Creation

--- src/cgp.py
+++ src/cgp.py
@@ -1,31 +1,47 @@
+# Python Standard Modules
 import os
 import sys
+import argparse
 
 try:
+    # Import 3rd-party Modules/Libraries
     from github import Github
     import github
     from dotenv import load_dotenv
     load_dotenv()
+
 except ModuleNotFoundError:
     print("Required Modules Not Installed...")
-    print("Run: pip install -r requirements.txt")
+    print("(Run): pip install -r requirements.txt")
     sys.exit(2)
 
-if len(sys.argv) not in list(range(3, 6)):
-    print("Usage: create-git-project [repository name] --license [license*] --repo-access [repository access*]")
-    sys.exit(1)
 
 def main():
+    """
+    Create-Git-Project
+    Main Function of the Program
+    """
 
-    print(os.environ["access_token"])
+    # Parse Command Line Arguments 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("repoName", help="Repository Name", type=str)
+    parser.add_argument("-l", "--license", help="Initialize Repository with (X) License", type=str)
+    parser.add_argument("-ra", "--repoAccess", help="Create a public or private repository. [DEFAULT: public]", type=str)
+    args = parser.parse_args()
+    print(args)
 
+    # Creata a Github Instance
     g = Github(os.environ["access_token"])
 
     # Verify Authentication
     if not verifyAuthentication(g):
-        print("Authentication Failed Verify your access token...")
+        print("(Authentication Failed): Please Verify your access token...")
         sys.exit(3)
 
+    # Get User 
+    user = g.get_user()
+    # Create A Repo Under User - Private/Public
+    repo = user.create_repo(args.repoName, private=True if args.repoAccess.lower() == "private" else False)
 
     
 def verifyAuthentication(g):
@@ -36,7 +52,7 @@ def verifyAuthentication(g):
     """
     try:
         g.get_user().login
-    except github.GithubException.BadCredentialsException:
+    except github.GithubException:
         return False
     return True
 
